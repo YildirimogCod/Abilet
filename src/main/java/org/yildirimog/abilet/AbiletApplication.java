a7ee4e20c2b0a304f7be238a440e874b50b43030
Oguzhan Yildirim <yildirimogcod@gmail.com>
2026-06-20 16:49:50 +0300

    feat: initial project setup with profiles

---
+++ src/main/java/org/yildirimog/abilet/AbiletApplication.java
@@ -0,0 +1,13 @@
+package org.yildirimog.abilet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbiletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbiletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/yildirimog/abilet/AbiletApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yildirimog.abilet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbiletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
