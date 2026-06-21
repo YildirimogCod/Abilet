8ee3a751413a19fe35bf55e027e580418396c169
Oguzhan Yildirim <yildirimogcod@gmail.com>
2026-06-21 17:08:35 +0300

    feat: add Comment entity

---
+++ src/main/java/org/yildirimog/abilet/comment/entity/Comment.java
@@ -0,0 +1,38 @@
+package org.yildirimog.abilet.comment.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.yildirimog.abilet.common.entity.BaseEntity;
+import org.yildirimog.abilet.event.entity.Event;
+import org.yildirimog.abilet.user.entity.User;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
+public class Comment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Event event;
+
+
+}
